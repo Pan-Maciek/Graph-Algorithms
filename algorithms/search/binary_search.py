af83a9028fefa0c0a457a1f5187f8e885888963e
koziejka <koziejka.com@gmail.com>
2019-10-15 21:28:54 +0200

    binary search over continuous range

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -7,12 +7,38 @@ def SelectAndLess(val):
 
 __sorted = sorted  # Used because of argument name
 
-def binary_search(iterable, selector, sorted=False):
+def binary_search(min, max, selector):
+    """
+    Used for values from continuous range (min, max)
+    """
+    L, R = min, max
+    selected = None
+
+    while L <= R:
+        m = (L + R) / 2
+        action = selector(m)
+        if action == NotFound: return NotFound
+        elif action == Found: return m
+        elif action == More: L = m + 1
+        elif action == Less: R = m - 1
+        elif action[0] == More:
+          selected = action[1]
+          L = m + 1
+        elif action[0] == Less:
+          selected = action[1]
+          R = m - 1
+
+    return selected
+
+def discrete_binary_search(iterable, selector, sorted=False):
+    """
+    Used for discrete values from set.
+    """
     iterable = list(iterable) if sorted else list(__sorted(iterable))
     L, R = 0, len(iterable) - 1
     selected = None
 
-    while(L <= R):
+    while L <= R:
         m = (L + R) // 2
         action = selector(iterable[m])
         if action == NotFound: return NotFound
