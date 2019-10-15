e2ce3717f580cff14201c94a30362d5e556f286b
koziejka <koziejka.com@gmail.com>
2019-10-15 22:21:36 +0200

    optimization

---
+++ max_flow_binary_search.py
@@ -0,0 +1,18 @@
+from structures.graph import weight
+from algorithms.search.binary_search import SelectAndMore, Less, binary_search
+from algorithms.search.dfs import dfs
+from algorithms.numeric.min_max import min_max
+from test_utils import test, load_graph
+
+# Finding max flow using binary search
+
+s, t = 1, 2
+
+@test(data="./data", loader=load_graph, times=100)
+def run(G):
+  def find_max_flow(flow):
+      return SelectAndMore(flow) if dfs(G, s, t, condition=lambda edge: weight(edge) >= flow) else Less
+  
+  min, max = min_max(map(weight, G.E))
+  max_flow = binary_search(min, max, find_max_flow)
+  return max_flow
