72751e67d8d6ede33379bebba0b34b7af6b381c3
Pan-Maciek <koziejka.com@gmail.com>
2019-10-19 17:06:09 +0200

    implemented max_flow_path using dijkstra's algorytm modification

---
+++ solutions/max_flow_path_dijkstra.py
@@ -0,0 +1,14 @@
+from test_utils import test, load_graph
+from algorithms.paths.dijkstra import dijkstra, high_priority, inf, Relax, SkipRelaxation
+
+# Finding path with max flow using disjoint sets
+
+def relax(u, v, w):
+    flow = min(u, w)
+    return Relax(flow) if flow > v else SkipRelaxation
+
+s, t = 1, 2
+@test(data="./data/lab1", loader=load_graph)
+def run(G):
+    max_flow = dijkstra(G, s, initial_distance=0, source_distance=inf, relax=relax, priority=high_priority)[t]
+    return max_flow
