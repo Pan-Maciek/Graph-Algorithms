977ea2f1cb2e6ea94af9baeb73e0fae9d7b8bac0
Pan-Maciek <koziejka.com@gmail.com>
2019-10-20 20:34:05 +0200

    implemented iter + path accumulation

--- structures/path.py
+++ structures/path.py
@@ -1,15 +1,35 @@
 class path(tuple):
-    def __new__(self, iter):
+    def __new__(self, iter, accumulated=None):
         return tuple.__new__(path, iter)
 
+    def __init__(self, iter, accumulated=None):
+        self.accumulated = accumulated
+
     def __str__(self):
-        return '(' + ' -> '.join(map(str, self)) + ')'
+        return ('(' if self.accumulated == None else '(acc: ' + str(self.accumulated) + '; ') + ' -> '.join(map(str, self)) + ')' 
+
+    def __iter__(self):
+        iterable = tuple.__iter__(self) 
+        u = next(iterable)
+        for v in iterable:
+            yield u, v
+            u = v
 
     @staticmethod
-    def from_previous_list(previous, index):
+    def from_previous_list(previous, index, accumulator=None, initial_acc_state=0):
         reversed_path = []
-        while previous[index] != None:
+        state = initial_acc_state
+        if previous[index] == None:
+            return None
+        if accumulator == None:
+            while previous[index] != None:
+                reversed_path.append(index)
+                index, _ = previous[index]
             reversed_path.append(index)
-            index = previous[index]
-        reversed_path.append(index)
-        return path(reversed(reversed_path))
+        else:
+            while previous[index] != None:
+                reversed_path.append(index)
+                index, w = previous[index]
+                state = accumulator(state, w)
+            reversed_path.append(index)
+        return path(reversed(reversed_path), accumulated = None if accumulator == None else state)
