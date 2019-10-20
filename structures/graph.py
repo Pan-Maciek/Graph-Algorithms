6b62e416919e06b529a61549294b2d86cea71f1a
Pan-Maciek <koziejka.com@gmail.com>
2019-10-20 20:34:51 +0200

    implemented residual_graph

--- structures/graph.py
+++ structures/graph.py
@@ -3,6 +3,7 @@ class numeric_graph(object):
         self.V = V
         self.E = E
         self._data = list([] for _ in range(V))
+        self._directed = directed
         if directed:
             for u, v, w in E:
                 self._data[u].append((v, w))
@@ -37,6 +38,30 @@ class numeric_graph(object):
         f.close()
         return numeric_graph(V, L, directed=directed)
 
+class residual_graph(object):
+    def __init__(self, base_graph):
+        self.V = base_graph.V
+        self._directed = base_graph._directed
+        self.weight = list([None for _ in range(base_graph.V)] for _ in range(base_graph.V))
+        if base_graph._directed:
+            for u, v, w in base_graph.E:
+                self.weight[u][v] = w
+        else:
+            for u, v, w in base_graph.E:
+                self.weight[u][v] = w
+                self.weight[v][u] = w
+    
+    def edges(self, u):
+        return ((v, w) for v, w in enumerate(self.weight[u]) if w != None)
+    
+    def neighbors(self, u):
+        return (v for v in range(self.V) if self.weight[v] != None)
+
+    def inc_weight(self, u, v, delta):
+        self.weight[u][v] = self.weight[u][v] + delta if self.weight[u][v] != None else delta
+
+    def dec_weight(self, u, v, delta):
+        self.weight[u][v] = self.weight[u][v] - delta if self.weight[u][v] != None else -delta
 
 def weight(edge):
     return edge[-1]
