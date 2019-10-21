f6f7c2324f9c7f99b8dfeebfbc808b3dd5328c6c
Pan-Maciek <koziejka.com@gmail.com>
2019-10-22 00:01:35 +0200

    fixed __str__, iteration over empty path

--- structures/path.py
+++ structures/path.py
@@ -6,14 +6,16 @@ class path(tuple):
         self.accumulated = accumulated
 
     def __str__(self):
-        return ('(' if self.accumulated == None else '(acc: ' + str(self.accumulated) + '; ') + ' -> '.join(map(lambda x: str(x[0]), self)) + ')' 
+        return ('(' if self.accumulated == None else '(acc: ' + str(self.accumulated) + '; ') + ' -> '.join(map(str, tuple.__iter__(self))) + ')' 
 
     def __iter__(self):
-        iterable = tuple.__iter__(self) 
-        u = next(iterable)
-        for v in iterable:
-            yield u, v
-            u = v
+        if bool(self):
+            iterable = tuple.__iter__(self) 
+            u = next(iterable)
+            
+            for v in iterable:
+                yield u, v
+                u = v
 
     @staticmethod
     def from_previous_list(previous, index, accumulator=None, initial_acc_state=0):
@@ -21,7 +23,6 @@ class path(tuple):
         state = initial_acc_state
         if previous[index] == None:
             return None
-        reversed_path.append(index)
         if accumulator == None:
             while previous[index] != None:
                 reversed_path.append(index)
